c7606cded000e3c5ed4798c0d7d1e5783cf81a3b
Nicolò Frison <nfrison@thorguard.com>
2024-05-01 14:38:41 -0400

    Add retrieve of places through mix between textSearch and searchNearby

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@
 
 import { Item } from "@/components/item";
 import Image from "next/image";
-import { textSearch } from "./api/googlePlaces";
+import { nearbySearchByNewTextSearch, textSearch } from "./api/googlePlaces";
 
 export default function Home() {
-  async function getPlace(textQuery: string) {
+  async function getPlaceByTextSearch(textQuery: string) {
     const text = textQuery.trim();
     if (text.length === 0) {
       return [];
@@ -25,10 +25,35 @@ export default function Home() {
     }
   }
 
+  async function getPlaceByTextSearchAndNearby(textQuery: string) {
+    const text = textQuery.trim();
+    if (text.length === 0) {
+      return [];
+    }
+
+    try {
+      const places = await nearbySearchByNewTextSearch(textQuery);
+
+      if (places.length === 0) {
+        return [];
+      }
+
+      return places;
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Item placesProvider={getPlace} />
+      <div className="w-full text-sm">
+        <h2>Get place by TextSearch api</h2>
+        <Item placesProvider={getPlaceByTextSearch} />
+      </div>
+      <div className="w-full text-sm">
+        <h2>Get place by mix between TextSearch and SearchNearby apis</h2>
+        <Item placesProvider={getPlaceByTextSearchAndNearby} />
       </div>
     </main>
   );
